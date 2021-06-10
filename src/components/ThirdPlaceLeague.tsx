c87733723500749ca99481732600511eb3024138
adam <20361733+rzencoder@users.noreply.github.com>
2021-06-10 15:59:36 +0100

    styling for knockout stages

--- src/components/ThirdPlaceLeague.tsx
+++ src/components/ThirdPlaceLeague.tsx
@@ -1,18 +1,27 @@
+import Flag from "react-flags";
+
 export default function ThirdPlaceLeague({ teams, calculateThirdPlaceLeague, positions }) {
     const getRanking = (name) => {
         const index = positions.findIndex(el => el.name === name) + 1
-        if (index === 0 && positions.length === 4) return "out"
-        return index === 0 ? "+" : index < 5 ? "qualified" : "out"
+        if (index === 0 && positions.length === 4) return <div className="cross">&#x2716;</div>
+        return index === 0 ? "" : index < 5 ? <div className="tick">&#x2714;</div> : <div className="cross">&#x2716;</div>
     }
     console.log(teams)
     return (
-        <div>
-            <h2>Third Place League</h2>
+        <div className="third-place-league">
             {teams.map(team => {
                 return (
-                    <div className="third-place-team" onClick={() => calculateThirdPlaceLeague(team)}>
-                        <div>{team.name}</div>
-                        <div>{getRanking(team.name)}</div>
+                    <div className="third-place-container" onClick={() => calculateThirdPlaceLeague(team)}>
+                        <div className="third-place-team">
+                            <Flag
+                                name={team.flag}
+                                format="svg"
+                                width="50"
+                                basePath="/img/flags"
+                                alt={`${team.name} flag`} />
+                            <div><div>{team.name}</div></div>
+                        </div>
+                        <div className="third-place-selector"><div>{getRanking(team.name)}</div></div>
                     </div>
                 )
             })}
